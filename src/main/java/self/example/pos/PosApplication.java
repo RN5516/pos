e318d55cc3f1cbaedec1d2cffdfdd8b38a914559
RN5516 <nageshritvik@gmail.com>
2024-11-25 12:50:16 -0600

    How are you? - added
    How are you? - added

--- src/main/java/self/example/pos/PosApplication.java
+++ src/main/java/self/example/pos/PosApplication.java
@@ -9,6 +9,7 @@ public class PosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PosApplication.class, args);
 		System.out.println("Welcome to Pos!");
+		System.out.println("How are you today?");
 	}
 
 }
